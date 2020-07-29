fd9c4b48023bbfe4411bcf6d88d3dfca01bc3f53
belmont95@live.com <dawn@DESKTOP-VMA33IJ.localdomain>
2020-07-29 05:27:13 -0500

    Adjusted number of newlines output per extracted tweet (2 to 1); added documentation

--- geotag_filter.py
+++ geotag_filter.py
@@ -5,8 +5,16 @@ Outputs the extracted Tweets in JSONL format
 import json
 
 def is_geotagged(tweet_json: str) -> bool:
+    """
+    Takes in a tweets JSON string and outputs whether the underlying tweet is geotagged.
+    Arguments:
+        tweet-json: str - the json string whose tweet is to be tested
+    Returns:
+        True if the underlying tweet in the provided string is geotagged (has a coordinates field),
+                false otherwise.
+    """
     tweet_dict = json.loads(tweet_json)
-    return tweet_json['coordinates'] is not None
+    return tweet_dict['coordinates'] is not None
 
 
 if __name__ == "__main__":
@@ -14,11 +22,11 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('input', metavar='I',
-        description="The JSONL tweets file to extract geotagged tweets from")
+        help="The JSONL tweets file whence to extract geotagged tweets")
     args = parser.parse_args()
 
     with open(args.input) as tweet_jsons:
         #Extract only the tweets that have a coordinate location associated to them
         for tweet_json in tweet_jsons:
             if is_geotagged(tweet_json):
-                print(tweet_json)
+                print(tweet_json, end="") #Newlines already conclude tweet strings themselves
