b68423f5ddd259f735d3363960443ae2979dc527
Dawn McKnight <demcknig@ualberta.ca>
2020-11-19 03:52:04 -0600

    Updating the geotag filter to use places

--- geotag_filter.py
+++ geotag_filter.py
@@ -4,20 +4,21 @@ Outputs the extracted Tweets in JSONL format
 """
 import json
 
-def is_geotagged(tweet_json: str, bounding_box_is_fine: bool=True) -> bool:
+def is_geotagged(tweet_json: str, place_is_fine: bool=True) -> bool:
     """
     Takes in a tweets JSON string and outputs whether the underlying tweet is geotagged.
     Arguments:
         tweet-json: str - the json string whose tweet is to be tested
     Returns:
-        True if the underlying tweet in the provided string is geotagged (has a bounding box
-                or coordinates field); false, otherwise.
+        True if the underlying tweet in the provided string is geotagged (has a place
+                (only an option if place_is_fine is flagged true) or a coordinates field);
+                false, otherwise.
     """
     tweet_dict = json.loads(tweet_json)
-    bounding_box_present = 'bounding_box' in tweet_dict and tweet_dict['bounding_box'] is not None
+    place_present = 'place' in tweet_dict and tweet_dict['place'] is not None
     coordinates_present = 'coordinates' in tweet_dict and tweet_dict['coordinates'] is not None
-    if bounding_box_is_fine:
-        is_geotagged_result = bounding_box_present or coordinates_present
+    if place_is_fine:
+        is_geotagged_result = place_present or coordinates_present
     else:
         is_geotagged_result = coordinates_present
     return is_geotagged_result
@@ -30,14 +31,14 @@ if __name__ == "__main__":
     parser.add_argument('input', metavar='I',
         help="The JSONL tweets file whence to extract geotagged tweets")
     parser.add_argument('-s', '--strict', action='store_true',
-        help="Whether to only allow tweets that have a non-null coordinates field (by default tweets with a bounding_box field are also allowed through")
+        help="Whether to only allow tweets that have a non-null coordinates field (by default tweets with a place field are also allowed through")
     args = parser.parse_args()
 
     with open(args.input) as tweet_jsons:
         #Extract only the tweets that have a coordinate location associated to them
         for tweet_json in tweet_jsons:
             try:
-                if is_geotagged(tweet_json):
+                if is_geotagged(tweet_json, !args.strict):
                     print(tweet_json, end="") #Newlines already conclude tweet strings themselves
             except ValueError:
                 pass
